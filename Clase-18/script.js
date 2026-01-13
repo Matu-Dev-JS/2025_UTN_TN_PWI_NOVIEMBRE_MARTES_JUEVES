de5a4bb4f888f6a9827b8cc36dd2682a97644ff8
matu.dev <mati.a.gimenez2002@gmail.com>
2026-01-13 20:08:02 -0300

    Clase 18

---
+++ Clase-18/script.js
@@ -0,0 +1,42 @@
+/* 
+DOM = Document Object Model 
+document es una variable global de tipo objeto que representa toda nuesta pagina
+Nos permite interactuar con el HTML de nuestra página
+El document naturalmente es un objeto GIGANTE, debido a que tiene que guardar en sus propiedades CADA caracteristica de la pagina
+
+document vs window
+Son interfaces para acceder a cosas distintas, document es para el DOM (osea la parte visible de la web), window es para el BOM (browser object model) y sirve para interactuar con el navegador
+*/
+
+
+console.dir(document)
+
+
+//method .getElementById()
+
+const btnSaludar = document.getElementById('btn-saludar')
+console.dir(btnSaludar)
+btnSaludar.innerText = 'chau mundo'
+
+let nombre = 'pepe'
+
+const tituloBienvenida = document.getElementById('titulo-bienvenida')
+
+tituloBienvenida.innerText = tituloBienvenida.innerText + ' ' + nombre
+
+
+
+/* Dado este objeto imprimir los datos en el HTML */
+const sesion = {
+    id: 1,
+    username: 'pepe',
+    role: 'admin',
+    email: 'pepe@gmail.com',
+    pendingNotifications: 6
+}
+
+const titulo = document.getElementById('titulo-primario');
+titulo.innerText = `Bienvenido ${sesion.username}`;
+
+const notificaciones = document.getElementById('notificaciones');
+notificaciones.innerText = `Tienes ${sesion.pendingNotifications} notificaciones revisa tu casilla electronica ${sesion.email}`;
