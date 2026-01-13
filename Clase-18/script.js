79da1ed05df7c7ce177b862967395e8bebd6efbf
matu.dev <mati.a.gimenez2002@gmail.com>
2026-01-13 20:58:18 -0300

    Clase 18

--- Clase-18/script.js
+++ Clase-18/script.js
@@ -82,3 +82,19 @@ condicion
 : console.log('La condicion no se cumplio')
 */
 
+const message = {
+    id: 1,
+    sender_name: 'pepe',
+    text: 'Hola que tal',
+    isVip: true,
+    isPinned: true,
+    attachments: [
+        'https://scontent.ffdo4-1.fna.fbcdn.net/v/t39.30808-6/480830852_1195733161920624_3504195831939762763_n.jpg?stp=dst-jpg_s640x640_tt6&_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeF71kLQYUCR_Gi8O-0uY_QkpFjlGkaInKykWOUaRoicrJz-sO4IXu_EEggveYKPDWnrvcP2O_y1zlaECtB5_kdV&_nc_ohc=kJbeISkFpSsQ7kNvwHyrJeD&_nc_oc=AdmOhv4EIxdYVULy7zh9JXz3-gJU3I8Nj7rx7bAcUAdZQFkI5aD74bnSb3iuQnXbXSBeLzzaNhnLaHFVttC8nyHR&_nc_zt=23&_nc_ht=scontent.ffdo4-1.fna&_nc_gid=8iEY8252STYCLmQWhR82iQ&oh=00_Afor4NGAgAgLGbA2aSts3O_3OT7_xHa5Po1qEQe0FC9pFg&oe=696CC732',
+
+    ]
+}
+
+//Renderizar el mensaje dentro de un div
+//Si el mennsaje es vip deberia tener la class='vip' que debe hacer algun cambio visual, por ejemplo el texto resaltado (text-shadow)
+//Si isPinned es verdadero el mensaje debera tener algun texto en alguna parte que indique que esta marcado (puede ser un icono o y mas recomendable un texto que diga 'marcado')
+//(Opcional) hacer el renderizado de los attachments (archivos adjuntos) abajo del mensaje (Aclaracion: Todos los attachments seran imagenes )
