d1defd90b50ab1c75290c1965aca182b13a20a97
matu.dev <mati.a.gimenez2002@gmail.com>
2026-01-13 22:00:16 -0300

    Clase 18

--- Clase-18/script.js
+++ Clase-18/script.js
@@ -44,11 +44,11 @@ notificaciones.innerText = `Tienes ${sesion.pendingNotifications} notificaciones
 
 const contenedor = document.getElementById('contenedor')
 
-function renderOferta (oferta){
-    if (oferta && oferta > 0 ){
-        return `<span>%${oferta} descuento</span>` 
+function renderOferta(oferta) {
+    if (oferta && oferta > 0) {
+        return `<span>%${oferta} descuento</span>`
     }
-    else{
+    else {
         return ''
     }
 }
@@ -57,15 +57,14 @@ let oferta = 10
 contenedor.innerHTML = `
     <hr/>
     <h2>Tv samsung</h2>
-    ${
-        oferta && oferta > 0 
-        ? 
-        `<span>%${oferta} descuento</span>` 
-        : 
+    ${oferta && oferta > 0
+        ?
+        `<span>%${oferta} descuento</span>`
+        :
         ''
     }
     ${renderOferta(oferta)}
-    <span>Precio: $${ 400 - (400 * oferta / 100)} usd</span>
+    <span>Precio: $${400 - (400 * oferta / 100)} usd</span>
     <br>
     <button>Comprar</button>
 `
@@ -82,7 +81,7 @@ condicion
 : console.log('La condicion no se cumplio')
 */
 
-const message = {
+/* const message = {
     id: 1,
     sender_name: 'pepe',
     text: 'Hola que tal',
@@ -92,9 +91,57 @@ const message = {
         'https://scontent.ffdo4-1.fna.fbcdn.net/v/t39.30808-6/480830852_1195733161920624_3504195831939762763_n.jpg?stp=dst-jpg_s640x640_tt6&_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeF71kLQYUCR_Gi8O-0uY_QkpFjlGkaInKykWOUaRoicrJz-sO4IXu_EEggveYKPDWnrvcP2O_y1zlaECtB5_kdV&_nc_ohc=kJbeISkFpSsQ7kNvwHyrJeD&_nc_oc=AdmOhv4EIxdYVULy7zh9JXz3-gJU3I8Nj7rx7bAcUAdZQFkI5aD74bnSb3iuQnXbXSBeLzzaNhnLaHFVttC8nyHR&_nc_zt=23&_nc_ht=scontent.ffdo4-1.fna&_nc_gid=8iEY8252STYCLmQWhR82iQ&oh=00_Afor4NGAgAgLGbA2aSts3O_3OT7_xHa5Po1qEQe0FC9pFg&oe=696CC732',
 
     ]
-}
+} */
 
 //Renderizar el mensaje dentro de un div
-//Si el mennsaje es vip deberia tener la class='vip' que debe hacer algun cambio visual, por ejemplo el texto resaltado (text-shadow)
+//Si el mensaje es vip deberia tener la class='vip' que debe hacer algun cambio visual, por ejemplo el texto resaltado (text-shadow)
 //Si isPinned es verdadero el mensaje debera tener algun texto en alguna parte que indique que esta marcado (puede ser un icono o y mas recomendable un texto que diga 'marcado')
 //(Opcional) hacer el renderizado de los attachments (archivos adjuntos) abajo del mensaje (Aclaracion: Todos los attachments seran imagenes )
+
+const mensaje = {
+    id: 1,
+    send_name: 'pepe',
+    text: 'Buen dia',
+    isVip: true,
+    isPinned: true,
+    attachments: [
+        'https://scontent.ffdo4-1.fna.fbcdn.net/v/t39.30808-6/480830852_1195733161920624_3504195831939762763_n.jpg?stp=dst-jpg_s640x640_tt6&_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeF71kLQYUCR_Gi8O-0uY_QkpFjlGkaInKykWOUaRoicrJz-sO4IXu_EEggveYKPDWnrvcP2O_y1zlaECtB5_kdV&_nc_ohc=kJbeISkFpSsQ7kNvwHyrJeD&_nc_oc=AdmOhv4EIxdYVULy7zh9JXz3-gJU3I8Nj7rx7bAcUAdZQFkI5aD74bnSb3iuQnXbXSBeLzzaNhnLaHFVttC8nyHR&_nc_zt=23&_nc_ht=scontent.ffdo4-1.fna&_nc_gid=8iEY8252STYCLmQWhR82iQ&oh=00_Afor4NGAgAgLGbA2aSts3O_3OT7_xHa5Po1qEQe0FC9pFg&oe=696CC732',
+        'https://scontent.ffdo4-1.fna.fbcdn.net/v/t39.30808-6/480830852_1195733161920624_3504195831939762763_n.jpg?stp=dst-jpg_s640x640_tt6&_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeF71kLQYUCR_Gi8O-0uY_QkpFjlGkaInKykWOUaRoicrJz-sO4IXu_EEggveYKPDWnrvcP2O_y1zlaECtB5_kdV&_nc_ohc=kJbeISkFpSsQ7kNvwHyrJeD&_nc_oc=AdmOhv4EIxdYVULy7zh9JXz3-gJU3I8Nj7rx7bAcUAdZQFkI5aD74bnSb3iuQnXbXSBeLzzaNhnLaHFVttC8nyHR&_nc_zt=23&_nc_ht=scontent.ffdo4-1.fna&_nc_gid=8iEY8252STYCLmQWhR82iQ&oh=00_Afor4NGAgAgLGbA2aSts3O_3OT7_xHa5Po1qEQe0FC9pFg&oe=696CC732',
+        'https://scontent.ffdo4-1.fna.fbcdn.net/v/t39.30808-6/480830852_1195733161920624_3504195831939762763_n.jpg?stp=dst-jpg_s640x640_tt6&_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeF71kLQYUCR_Gi8O-0uY_QkpFjlGkaInKykWOUaRoicrJz-sO4IXu_EEggveYKPDWnrvcP2O_y1zlaECtB5_kdV&_nc_ohc=kJbeISkFpSsQ7kNvwHyrJeD&_nc_oc=AdmOhv4EIxdYVULy7zh9JXz3-gJU3I8Nj7rx7bAcUAdZQFkI5aD74bnSb3iuQnXbXSBeLzzaNhnLaHFVttC8nyHR&_nc_zt=23&_nc_ht=scontent.ffdo4-1.fna&_nc_gid=8iEY8252STYCLmQWhR82iQ&oh=00_Afor4NGAgAgLGbA2aSts3O_3OT7_xHa5Po1qEQe0FC9pFg&oe=696CC732',
+        'https://scontent.ffdo4-1.fna.fbcdn.net/v/t39.30808-6/480830852_1195733161920624_3504195831939762763_n.jpg?stp=dst-jpg_s640x640_tt6&_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeF71kLQYUCR_Gi8O-0uY_QkpFjlGkaInKykWOUaRoicrJz-sO4IXu_EEggveYKPDWnrvcP2O_y1zlaECtB5_kdV&_nc_ohc=kJbeISkFpSsQ7kNvwHyrJeD&_nc_oc=AdmOhv4EIxdYVULy7zh9JXz3-gJU3I8Nj7rx7bAcUAdZQFkI5aD74bnSb3iuQnXbXSBeLzzaNhnLaHFVttC8nyHR&_nc_zt=23&_nc_ht=scontent.ffdo4-1.fna&_nc_gid=8iEY8252STYCLmQWhR82iQ&oh=00_Afor4NGAgAgLGbA2aSts3O_3OT7_xHa5Po1qEQe0FC9pFg&oe=696CC732',
+        
+    ]
+}
+//renderizar el mensaje dentro de un div
+//si el mensaje es vip deveria tener la class='vip' 
+//si el mensaje es isPinned deberra tener un texto que diga 'importante'
+//hacer el renderizado de los attachments
+const mensaje_HTML = document.getElementById("mensaje")
+
+function renderAttachments (attachments){
+    let html = ''
+    for(const attachment of attachments){
+        html += `<img src="${attachment}">`
+    }
+    return html
+}
+
+mensaje_HTML.innerHTML = `
+        ${
+            mensaje.isPinned
+            ?
+            `<span>¡importante!</span>`
+            :
+            ``
+        }
+        <br>
+        <span 
+            class='message ${mensaje.isVip ? 'vip' : ''}'
+        >
+            ${mensaje.text}
+        </span>
+        <br>
+        <div>
+            ${renderAttachments(mensaje.attachments)}
+        </div>
+    `
