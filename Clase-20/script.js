789a19dd6e20867ba602777ba785b8b763a1ff76
matu.dev <mati.a.gimenez2002@gmail.com>
2026-01-20 20:47:14 -0300

    Clase 20

--- Clase-20/script.js
+++ Clase-20/script.js
@@ -12,10 +12,55 @@ consultaAlServidor() ({estado: "pending" ('resolved', 'rejected')})
 La funcion fetch nos permite ordenar al navegador que ejecute una consulta HTTP a x direccion
 */
 
-let estado_tareas = null
 
-async function consultarAUnaAPI (){
-    console.log('hola')
+//El lugar en memoria donde vamos a guardar como esta nuestra app
+let estado_tareas = {
+    tareas: null,
+    cargando: false,
+    error: null
+}
+
+const contenedor_principal = document.getElementById('contenedor-principal')
+
+function renderTareas (){
+    console.log(estado_tareas)
+    if( estado_tareas.cargando){
+        contenedor_principal.innerHTML = `<span>Cargando...</span>`
+        return 
+    }
+    if(estado_tareas.error){
+        contenedor_principal.innerHTML = `<span>Hubo un error al cargar la lista, prueba reintentar</span>`
+        return
+    }
+    if(estado_tareas.tareas && estado_tareas.tareas.length === 0){
+        contenedor_principal.innerHTML = `<span>No hay tareas</span>`
+        return
+    }
+    if(!estado_tareas.tareas){
+        return
+    }
+    let html = ''
+    for(let tarea of estado_tareas.tareas){
+        html = html + `
+        <div>
+            <h2>${tarea.title}</h2>
+            <span>Estatus: ${tarea.completed ? 'Completada' : 'Pendiente'}</span>
+        </div>
+        `
+    }
+
+    contenedor_principal.innerHTML = html
+}
+
+renderTareas()
+
+function setTareas (valor){
+    estado_tareas = valor
+    renderTareas()
+}
+
+async function cargarTareas (){
+    setTareas({...estado_tareas, cargando: true})
     const respuesta_HTTP = await fetch(
         'https://jsonplaceholder.typicode.com/todos',
         {
@@ -23,14 +68,12 @@ async function consultarAUnaAPI (){
         }
     )
 
-    console.log({respuesta_HTTP})
-
     //Transformamos la carga util y leemos como JSON
     const respuesta = await respuesta_HTTP.json()
-    estado_tareas = respuesta
+    setTareas({...estado_tareas, cargando: false, tareas: respuesta})
+    
 }
 
 
-consultarAUnaAPI()
-console.log("operacion super importante")
-console.log("Operacion 2")
+renderTareas()
+cargarTareas()
