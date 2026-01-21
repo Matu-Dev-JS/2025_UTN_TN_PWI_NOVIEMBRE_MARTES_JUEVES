243d70360b26f5ebec357a4692eef74dd16f5408
matu.dev <mati.a.gimenez2002@gmail.com>
2026-01-20 21:52:00 -0300

    clase 20

--- Clase-20/script.js
+++ Clase-20/script.js
@@ -14,7 +14,7 @@ La funcion fetch nos permite ordenar al navegador que ejecute una consulta HTTP 
 
 
 //El lugar en memoria donde vamos a guardar como esta nuestra app
-let estado_tareas = {
+/* let estado_tareas = {
     tareas: null,
     cargando: false,
     error: null
@@ -22,25 +22,25 @@ let estado_tareas = {
 
 const contenedor_principal = document.getElementById('contenedor-principal')
 
-function renderTareas (){
+function renderTareas() {
     console.log(estado_tareas)
-    if( estado_tareas.cargando){
+    if (estado_tareas.cargando) {
         contenedor_principal.innerHTML = `<span>Cargando...</span>`
-        return 
-    }
-    if(estado_tareas.error){
-        contenedor_principal.innerHTML = `<span>Hubo un error al cargar la lista, prueba reintentar</span>`
         return
     }
-    if(estado_tareas.tareas && estado_tareas.tareas.length === 0){
+    if (estado_tareas.error) {
+        contenedor_principal.innerHTML = `<span class="error">Hubo un error al cargar la lista, prueba reintentar</span>`
+        return
+    }
+    if (estado_tareas.tareas && estado_tareas.tareas.length === 0) {
         contenedor_principal.innerHTML = `<span>No hay tareas</span>`
         return
     }
-    if(!estado_tareas.tareas){
+    if (!estado_tareas.tareas) {
         return
     }
     let html = ''
-    for(let tarea of estado_tareas.tareas){
+    for (let tarea of estado_tareas.tareas) {
         html = html + `
         <div>
             <h2>${tarea.title}</h2>
@@ -54,29 +54,33 @@ function renderTareas (){
 
 renderTareas()
 
-function setTareas (valor){
+function setTareas(valor) {
     estado_tareas = valor
     renderTareas()
 }
 
-async function cargarTareas (){
-    setTareas({...estado_tareas, cargando: true})
-    const respuesta_HTTP = await fetch(
-        'https://jsonplaceholder.typicode.com/todos',
-        {
-            method: 'GET'
-        }
-    )
-
-    //Transformamos la carga util y leemos como JSON
-    const respuesta = await respuesta_HTTP.json()
-    setTareas({...estado_tareas, cargando: false, tareas: respuesta})
+async function cargarTareas() {
+    try{
+        setTareas({ ...estado_tareas, cargando: true })
+        const respuesta_HTTP = await fetch(
+            'https://jsonplaceholder.typicode.com/todos',
+            {
+                method: 'GET'
+            }
+        )
     
+        //Transformamos la carga util y leemos como JSON
+        const respuesta = await respuesta_HTTP.json()
+        setTareas({ ...estado_tareas, cargando: false, tareas: respuesta })
+    }
+    catch(error){
+        setTareas({ ...estado_tareas, cargando: false, error })
+    }
 }
 
 
 renderTareas()
-cargarTareas()
+cargarTareas() */
 
 /* 
 Consumir una API
@@ -86,3 +90,61 @@ Imprimir en pantalla un listado de nombres de los vehiculos, modelo y precio (Pu
 Imprimir en pantalla el cargando mientras se esten obteniendo los datos del servidor
 
 */
+
+
+let estado_vehiculos = {
+    vehiculos: null,
+    cargando: false,
+    error: null
+}
+const contenedor_principal = document.getElementById('contenedor-principal')
+function renderVehicules(){
+    if(estado_vehiculos.cargando){
+        contenedor_principal.innerHTML = `<span>Cargando...</span>`
+        return 
+    }
+    if(estado_vehiculos.error){
+        contenedor_principal.innerHTML = `<span>Hubo un error al cargar la lista, prueba reintentar</span>`
+        return
+    }
+    if(estado_vehiculos.vehiculos && estado_vehiculos.vehiculos.length === 0){
+        contenedor_principal.innerHTML = `<span>No hay vehiculos</span>`
+        return
+    }
+    if(!estado_vehiculos.vehiculos){
+        return
+    }
+    let html = ''
+    for(let vehiculo of estado_vehiculos.vehiculos){
+        html = html + `
+        <div>
+            <h2>${vehiculo.name}</h2>
+            <span>Modelo: ${vehiculo.model}</span>.
+            <br>
+            <span>Precio: ${vehiculo.cost_in_credits === "unknown" ? "Desconocido" : vehiculo.cost_in_credits}</span>
+        </div>
+        `
+    }
+    contenedor_principal.innerHTML = html
+}
+renderVehicules()
+function setVehiculos (valor){
+    estado_vehiculos = valor
+    renderVehicules()
+}
+
+
+async function cargaVehiculo (){
+    setVehiculos({...estado_vehiculos, cargando: true})
+    const respuesta_HTTP = await fetch(
+        'https://swapi.info/api/vehicles',
+        {
+            method: 'GET'
+        }
+    )
+    const respuesta = await respuesta_HTTP.json()
+    setVehiculos({...estado_vehiculos, cargando: false, vehiculos: respuesta})
+    
+}
+renderVehicules()
+cargaVehiculo()
