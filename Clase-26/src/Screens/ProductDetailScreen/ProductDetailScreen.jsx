0a9d0f43bfb37216cfd7115652feade88b8c4216
matu.dev <mati.a.gimenez2002@gmail.com>
2026-02-10 21:55:20 -0300

    Clase 26

--- Clase-26/src/Screens/ProductDetailScreen/ProductDetailScreen.jsx
+++ Clase-26/src/Screens/ProductDetailScreen/ProductDetailScreen.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import { useParams } from 'react-router'
+import { products } from '../HomeScreen/HomeScreen'
 
 export default function ProductDetailScreen() {
     /* 
     useParams es un hook de react-router que nos trae un objeto con todos los parametros de la url
     */
+    // /product/1 => {product_id: 1}
+    // /product/hola => {product_id: 'hola'}
     const objeto = useParams()
     console.log(objeto)
+
+    const product_selected = products.find(
+        (product) => {
+            return Number(product.id) === Number(objeto.product_id)
+        }
+    )
+
+    
   return (
     <div>
-        <h1>Detalle del producto</h1>
+        <h1>
+            Detalle del producto: {product_selected.title}
+        </h1>
     </div>
   )
 }
