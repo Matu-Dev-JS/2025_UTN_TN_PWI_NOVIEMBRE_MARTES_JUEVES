2c6b9260f4628a21617e4861459257d884ff7146
matu.dev <mati.a.gimenez2002@gmail.com>
2026-02-12 19:57:54 -0300

    Clase 27

--- Clase-27/src/App.jsx
+++ Clase-27/src/App.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import HomeScreen from './Screens/HomeScreen/HomeScreen'
+import ContactScreen from './Screens/ContactScreen/ContactScreen'
 
 
 function App() {
 
 
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <Routes>
+                <Route
+                    path='/'
+                    element={
+                        <HomeScreen />
+                    }
+                />
+                <Route
+                    path='/contact/:contact_id'
+                    element={
+                        <ContactScreen />
+                    }
+                />
+            </Routes>
+        </div>
+    )
 }
 
 export default App
